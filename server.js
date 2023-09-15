6e4e300038cbb6e85f4137a15b48152bf0b011db
Shreya Lahiri <shreyalahirisp@gmail.com>
2023-09-15 18:08:26 +0530

    Folder structure modified

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const path = require('path');
 const fs = require("fs");
 const multer = require("multer");
 
-const Product = require('./models/productModel')
+var productsRouter = require('./routes/products');
+
 const imgSchema = require('./models/imageModel')
 
 const app = express()
@@ -17,7 +18,11 @@ app.use(bodyParser.urlencoded(
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+app.use('/products', productsRouter);
+
 app.set("view engine", "ejs");
+
+
 
 var storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -66,65 +71,6 @@ mongoose.connect('mongodb+srv://shreyalahirisp:sushi080693@cluster0.yykawl0.mong
             });
         });
 
-        app.get('/products', async(req, res) => {
-            try {
-                const products = await Product.find({});
-                res.status(200).json(products);
-            } catch (error) {
-                res.status(500).json({message: error.message})
-            }
-        })
-
-        app.get('/products/:id', async(req, res) =>{
-            try {
-                const {id} = req.params;
-                const product = await Product.findById(id);
-                res.status(200).json(product);
-            } catch (error) {
-                res.status(500).json({message: error.message})
-            }
-        })
-
-        app.post('/products', async(req, res) => {
-            try {
-                const product = await Product.create(req.body)
-                res.status(200).json(product);
-                
-            } catch (error) {
-                console.log(error.message);
-                res.status(500).json({message: error.message})
-            }
-        })
-
-        app.put('/products/:id', async(req, res) => {
-            try {
-                const {id} = req.params;
-                const product = await Product.findByIdAndUpdate(id, req.body);
-                // we cannot find any product in database
-                if(!product){
-                    return res.status(404).json({message: `cannot find any product with ID ${id}`})
-                }
-                const updatedProduct = await Product.findById(id);
-                res.status(200).json(updatedProduct);
-                
-            } catch (error) {
-                res.status(500).json({message: error.message})
-            }
-        })
-
-        app.delete('/products/:id', async(req, res) =>{
-            try {
-                const {id} = req.params;
-                const product = await Product.findByIdAndDelete(id);
-                if(!product){
-                    return res.status(404).json({message: `cannot find any product with ID ${id}`})
-                }
-                res.status(200).json(product);
-                
-            } catch (error) {
-                res.status(500).json({message: error.message})
-            }
-        })
 
         app.listen(3000, () => {
             console.log('Node API is running on port 3000')
