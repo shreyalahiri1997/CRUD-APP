096b025a8ddd6c535daf4ac24f6e791f318c6d93
Shreya Lahiri <shreyalahirisp@gmail.com>
2023-09-15 17:33:33 +0530

    Image Adding

--- server.js
+++ server.js
@@ -1,19 +1,70 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const path = require('path');
+const fs = require("fs");
+const multer = require("multer");
+
 const Product = require('./models/productModel')
+const imgSchema = require('./models/imageModel')
 
 const app = express()
+
+app.use(bodyParser.urlencoded(
+    { extended:true }
+))
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+app.set("view engine", "ejs");
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+ 
+var upload = multer({ storage: storage });
+
 mongoose.connect('mongodb+srv://shreyalahirisp:sushi080693@cluster0.yykawl0.mongodb.net/NODE-API?retryWrites=true&w=majority')
     .then(() => {
         console.log('Connected!')
-        //routes
-        app.get('/', function (req, res) {
-            res.send('Hello from NODE API')
-        })
+        
+        app.get('/', (req, res) => {
+            imgSchema.find({})
+            .then((data, err)=>{
+                if(err){
+                    console.log(err);
+                }
+                res.render('imagepage',{items: data})
+            })
+        });
+
+        app.post('/', upload.single('image'), (req, res, next) => {
+ 
+            var obj = {
+                name: req.body.name,
+                desc: req.body.desc,
+                img: {
+                    data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+                    contentType: 'image/png'
+                }
+            }
+            imgSchema.create(obj)
+            .then ((err, item) => {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    // item.save();
+                    res.redirect('/');
+                }
+            });
+        });
 
         app.get('/products', async(req, res) => {
             try {
