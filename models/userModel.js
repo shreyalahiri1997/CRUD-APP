fff014ac9aed88ff265c55f8f30f657be04a6511
Shreya Lahiri <shreyalahirisp@gmail.com>
2023-09-16 14:22:00 +0530

    All endpoints

---
+++ models/userModel.js
@@ -0,0 +1,72 @@
+const mongoose = require('mongoose')
+
+// {
+//     "name": "Sneha Lahiri",
+//     "phone": "7003071463",
+//     "email": "snehalahiri09@gmail.com",
+//     "password": "sneha",
+//     "city": "Kolkata",
+//     "state": "West Bengal",
+//     "dob": "09-04-2004",
+//     "userRole": "admin",
+//     "storyProgress": [
+//         {
+//             "id": "650560ea793507b0fe8f3012",
+//             "score": 20,
+//             "levelCompleted": 2 
+//         }
+//     ]
+// }
+
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please enter a user name"]
+        },
+        phone: {
+            type: String,
+            required: [true, "Please enter a phone number"]
+        },
+        email: {
+            type: String,
+            required: [true, "Please enter a email"]
+        },
+        password: {
+            type: String,
+            required: [true, "Please enter a password"]
+        },
+        city: {
+            type: String,
+            default: ""
+        },
+        state: {
+            type: String,
+            default: ""
+        },
+        dob: {
+            type: String,
+            required: [true, "Please enter date of birth"]
+        },
+        quizProgress: {
+            type: Array,
+            default: []
+        },
+        storyProgress: {
+            type: Array,
+            default: []
+        },
+        userRole: {
+            type: String,
+            required: [true, "Please enter user role"]
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+
+const user = mongoose.model('user', userSchema);
+
+module.exports = user;
--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const fs = require("fs");
 const multer = require("multer");
 
 var productsRouter = require('./routes/products');
+var articlesRouter = require('./routes/article');
+var storiesRouter = require('./routes/story');
+var quizesRouter = require('./routes/quiz');
+var usersRouter = require('./routes/user');
 
 const imgSchema = require('./models/imageModel')
 
@@ -19,6 +23,10 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/products', productsRouter);
+app.use('/articles', articlesRouter);
+app.use('/stories', storiesRouter);
+app.use('/quizes', quizesRouter);
+app.use('/users', usersRouter);
 
 app.set("view engine", "ejs");
 
@@ -35,13 +43,14 @@ var storage = multer.diskStorage({
  
 var upload = multer({ storage: storage });
 
+// mongoose.connect('mongodb+srv://shreyalahirisp:sushi080693@cluster0.yykawl0.mongodb.net/NODE-API?retryWrites=true&w=majority')
 mongoose.connect('mongodb+srv://sneha:sneha@cluster0.rodg8nr.mongodb.net/?retryWrites=true&w=majority')
-    .then(() => {
+.then(() => {
         console.log('Connected!')
-        
+
         app.get('/', (req, res) => {
-            imgSchema.find({})
-            .then((data, err)=>{
+          imgSchema.find({})
+          .then((data, err)=>{
                 if(err){
                     console.log(err);
                 }
